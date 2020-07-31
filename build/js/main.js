725082c63a449ff9f1bc48df759ad37007b7e10a
Amelina_yuliia <amelinayullia@gmail.com>
2020-08-01 00:27:49 +0300

    делает правки после ревью

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,11 @@
-const subscriptionTimeElements = document.querySelectorAll('[data-tab]');
-const tabs = document.querySelectorAll('[data-list]');
+var subscriptionTimeElements = document.querySelectorAll('[data-tab]');
+var tabs = document.querySelectorAll('[data-list]');
 
-const handleTabClick = (evt) => {
+var handleTabClick = (evt) => {
   tabs.forEach(item => item.classList.remove('active'));
   subscriptionTimeElements.forEach(item => item.classList.remove('active'));
 
-  const activeTab = document.querySelector(`[data-list="${evt.target.dataset.tab}"]`);
+  var activeTab = document.querySelector(`[data-list="${evt.target.dataset.tab}"]`);
 
   evt.target.classList.add('active');
   activeTab.classList.add('active');
