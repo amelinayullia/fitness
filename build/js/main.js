8f9a808f89a9e50026c038f251c5e7048db4248e
Amelina_yuliia <amelinayullia@gmail.com>
2020-08-08 09:57:47 +0300

    подключает валидацию для формы

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,9 @@
 var subscriptionLinks = document.querySelectorAll(".subscription__time a");
 var subscriptionBlocks = document.querySelectorAll(".subscription__about-tab");
+var form = document.querySelector('.wrapper__form-inner form');
+var nameInput = document.querySelector('#input-name');
+var phoneInput = document.querySelector('#input-phone');
+var numInputsRegEx = /[^\d,+,\-,()]/g;
 
 var hideLinksActiveClass = function () {
   Array.prototype.forEach.call(subscriptionLinks, function (link) {
@@ -52,3 +56,34 @@ var reviewsSliders = new Swiper('.reviews__block', {
     prevEl: ".reviews__button-previous",
   },
 })
+
+var checkInputValidity = function(input) {
+  if (input.value) {
+    return true;
+  }
+  input.classList.add('error');
+  return false;
+}
+var onInputChangeHandler = function(e) {
+  if (e.target.classList.contains('error')) {
+    e.target.classList.remove('error');
+  }
+}
+var onPhoneInputChangeHandler = function(e) {
+  e.target.value = e.target.value.replace(numInputsRegEx, '');
+}
+var onFormSubmitHandler = function (e) {
+  e.preventDefault();
+  checkInputValidity(nameInput);
+  checkInputValidity(phoneInput);
+  if (checkInputValidity(nameInput) && checkInputValidity(phoneInput)) {
+    form.reset();
+  }
+}
+if (form) {
+  form.noValidate = true;
+  form.addEventListener('submit', onFormSubmitHandler);
+  phoneInput.addEventListener('input', onInputChangeHandler);
+  phoneInput.addEventListener('input', onPhoneInputChangeHandler);
+  nameInput.addEventListener('input', onInputChangeHandler);
+}
